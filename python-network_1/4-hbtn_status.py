ff69c5d8384b78bf932c413e6907de5613248436
Noelngerem <e.ngerem@alustudent.com>
2023-04-07 11:08:57 -0700

    task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" script that fetches https://alx-intranet.hbtn.io/status """
+""" script that fetches https://alu-intranet.hbtn.io/status """
 import requests
 
 
 def fetch_r():
-    """ fetches https://alx-intranet.hbtn.io/status """
-    req = requests.get('https://alx-intranet.hbtn.io/status')
+    """ fetches https://alu-intranet.hbtn.io/status """
+    req = requests.get('https://alu-intranet.hbtn.io/status')
     s = "Body response:\n\t- type: {}\n\t- content: {}"
     print(s.format(type(req.text), req.text))
 
