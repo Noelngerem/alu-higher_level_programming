dbb25a56a58b8180645d99e47694313429d39854
Noelngerem <e.ngerem@alustudent.com>
2023-06-08 12:09:40 -0700

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv;
-let request = require('request');
+const request = require('request');
 request(args[2], function (error, response, body) {
   if (error) {
     console.log('error:', error);
